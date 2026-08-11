83c7e58247c1f4382184ad50fc929ac50cfd5f04
YUCONGGEN <YUCONGGEN@users.noreply.github.com>
2026-08-11 11:46:12 +0800

    release: v2.0.1

--- spring/__init__.py
+++ spring/__init__.py
@@ -5,7 +5,7 @@ from .config import *
 from .utils import *
 from .main import create_app, run, SpringApplication, run_cli
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __author__ = "yuconggen"
 __license__ = "MIT"
 
